66c4888cff93f4fa0f2eba6118045cb46a2ac2dd
iGonoe <107329492+iGonoe@users.noreply.github.com>
2020-11-03 18:54:59 +0000

    Fixed codacy issues

--- project/frontend/scripts/jquery.js
+++ project/frontend/scripts/jquery.js
@@ -1,2 +1,2 @@
 /*! jQuery v3.4.1 | (c) JS Foundation and other contributors | jquery.org/license */
-!function(e,t){"use stri
+!function(e,t){"use strict";"object"==typeof module&&"object"==typeof module.exports?module.exports=e.document?t(e,!0):function(e){if(!e
